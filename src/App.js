3ac64fb4395f8b0e609bc8b80619be19ba84061b
nikitabarnawal <nbarnwal@kathmandu>
2021-04-28 12:27:31 +0545

    First commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h1", {}, animal),
+    React.createElement("h1", {}, breed),
+  ]);
+};
+
+const x = 5;
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "something-important" }, "Adopt Me"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cocktail",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
